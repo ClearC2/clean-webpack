06431bb4d80eb73feb8ef9ed99dd6e93fd5ef80e
Nick Swope <nswope@clearc2.com>
2018-01-26 18:24:43 -0600

    added proper root config to webpack-clean options

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-
-
 module.exports = function (plugins, pathsToClean = [], cleanOptions = {}) {
-  const defaultPathsToClean = [process.cwd() + '/dist/*.*']
-  const defaultCleanOptions = {exclude: '.gitignore'}
+  const defaultPathsToClean = ['dist/*.*']
+  const defaultCleanOptions = {
+    root: process.cwd()
+    exclude: '.gitignore'
+  }
 
   const combinedPathsToClean = defaultPathsToClean.concat(pathsToClean)
   const combinedCleanOptions = Object.assign(defaultCleanOptions, cleanOptions)
 
   plugins.push(new CleanObsoleteChunks())
   plugins.push(new CleanWebpackPlugin(combinedPathsToClean, combinedCleanOptions))
-
-  console.log(plugins, defaultPathsToClean)
 }
