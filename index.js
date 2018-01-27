3b6120699966eb84d825444f4fee478f82fc2dde
Nick Swope <nswope@clearc2.com>
2018-01-26 18:17:19 -0600

    made clean-webpack-plugin look at correct directory

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
 const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const defaultPathsToClean = ['dist/*.*']
+const defaultPathsToClean = [process.cwd() + '/dist/*.*']
 const defaultCleanOptions = {exclude: '.gitignore'}
 
 module.exports = function (plugins, pathsToClean = [], cleanOptions = {}) {
   const combinedPathsToClean = defaultPathsToClean.concat(pathsToClean)
   const combinedCleanOptions = Object.assign(defaultCleanOptions, cleanOptions)
-
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-
-  console.log('__dirname:', __dirname)
-  console.log('process.cwd:', process.cwd())
-
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
-  console.log('=========================================================')
 
   plugins.push(new CleanObsoleteChunks())
   plugins.push(new CleanWebpackPlugin(combinedPathsToClean, combinedCleanOptions))
