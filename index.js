9ee2a367092b6f802693eed29021379c0fc51b52
Nick Swope <nswope@clearc2.com>
2018-01-26 18:21:19 -0600

    added debugging console logs

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const defaultPathsToClean = [process.cwd() + '/dist/*.*']
-const defaultCleanOptions = {exclude: '.gitignore'}
+
 
 module.exports = function (plugins, pathsToClean = [], cleanOptions = {}) {
+  const defaultPathsToClean = [process.cwd() + '/dist/*.*']
+  const defaultCleanOptions = {exclude: '.gitignore'}
+
   const combinedPathsToClean = defaultPathsToClean.concat(pathsToClean)
   const combinedCleanOptions = Object.assign(defaultCleanOptions, cleanOptions)
 
   plugins.push(new CleanObsoleteChunks())
   plugins.push(new CleanWebpackPlugin(combinedPathsToClean, combinedCleanOptions))
 
-  console.log(plugins)
+  console.log(plugins, defaultPathsToClean)
 }
